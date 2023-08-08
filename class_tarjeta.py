f2215374ef4f3d01a0bd8ff9f95d9e3ffdf67a52
Alejandro Diez <90862187+alexdiezg@users.noreply.github.com>
2023-08-08 19:49:44 +0100

    Create user input for the ans & import to main

--- class_tarjeta.py
+++ class_tarjeta.py
@@ -1,11 +1,15 @@
 # This file will define and handle the card class
 
-class Card():
+class Card:
     def __innit__(self, question, answer, category):
         self.quesiton = question
         self.answer = answer
         self.category = category
-    
-    print("Soy una tarjeta")
-
-Card("España?", "Si", "Obvio")
+    def userInput():
+        ans = input("Answer: ")
+        try:
+            str(ans)
+            ans.strip().lower()
+            print(ans)
+        except:
+            print("Please enter a valid answer!")
